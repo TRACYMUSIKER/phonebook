c8b7dca5d4210234f74bd2e557351f33848144e7
TRACY S MUSIKER <tracymusiker@TRACYs-MacBook-Air.local>
2018-07-03 11:58:50 -0400

    first copy of phonebook project

---
+++ phonebookfunctions.py
@@ -0,0 +1,27 @@
+
+
+def phonebooklookup(names_phone_dict):
+        user_inputted_name = raw_input("Whose phone number would you like me to look up? ")
+        print "Found entry for %s: %s" % (user_inputted_name, names_phone_dict[user_inputted_name])
+    
+# def phonebookadd()    
+#     
+#         user_inputted_name = raw_input("What is the name of the person you'd like to add? ")
+#         user_inputted_number = raw_input("What is the phone number of the person you'd like to add? ")
+#         names_phone_dict[user_inputted_name] = user_inputted_number
+#         print "Entry stored for %s" % user_inputted_name
+
+# def phonebookdelete()
+#
+#         user_name_delete = raw_input("What is the name of the person you'd like to delete? ")
+#         del names_phone_dict[user_name_delete]
+#         print "Deleted entry for %s" % user_name_delete
+
+# def phonebooklistall()
+#    
+#         str(names_phone_dict)
+
+# def phonebookquit()
+#    
+#         print "Bye."
+   
